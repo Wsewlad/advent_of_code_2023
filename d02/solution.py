bfa352b623444ad2219a3a48ad8b35d0352a86bf
Vladyslav Fil <wladyslawfil@gmail.com>
2023-12-02 12:12:51 +0200

    Complete part 2 of d02

--- d02/solution.py
+++ d02/solution.py
@@ -17,10 +17,10 @@ def calculate_sum_of_game_ids(input_text: str) -> int:
     return result
 
 
-with open('input.txt', "r") as file:
-    text = file.read()
-    sum_of_game_ids = calculate_sum_of_game_ids(text)
-    print(sum_of_game_ids)
+# with open('input.txt', "r") as file:
+#     text = file.read()
+#     sum_of_game_ids = calculate_sum_of_game_ids(text)
+#     print(sum_of_game_ids)
 
 sample = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -29,6 +29,29 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 """
 
-sum_of_game_ids = calculate_sum_of_game_ids(sample)
-print(sum_of_game_ids)
+# sum_of_game_ids = calculate_sum_of_game_ids(sample)
+# print(sum_of_game_ids)
 
+
+def calculate_sum_of_power_of_minimum_sets_of_cubes(input_text: str) -> int:
+    result = 0
+    lines = input_text.splitlines()
+    for line in lines:
+        game, sets = line.split(":")
+        cubes = sets.replace(";", ",").split(",")
+        colors = ["red", "green", "blue"]
+        power = 1
+        for color in colors:
+            cube_numbers_of_color = sorted([int(cube.split()[0]) for cube in cubes if color in cube])
+            power *= cube_numbers_of_color[-1]
+        result += power
+    return result
+
+
+sum_of_power = calculate_sum_of_power_of_minimum_sets_of_cubes(sample)
+print(sum_of_power)
+
+with open('input.txt', "r") as file:
+    text = file.read()
+    sum_of_power = calculate_sum_of_power_of_minimum_sets_of_cubes(text)
+    print(sum_of_power)
