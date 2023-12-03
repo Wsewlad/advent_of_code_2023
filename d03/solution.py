573cdf2d24c3e47cd8ee6e69d664d23a50e3d7ff
Vladyslav Fil <wladyslawfil@gmail.com>
2023-12-03 14:08:45 +0200

    Still need to find and fix some edge case for part 1 of d03

---
+++ d03/solution.py
@@ -0,0 +1,57 @@
+import re
+
+
+def is_number_valid(span: tuple, lines: list) -> bool:
+    symbols_patter = r'[^\.\d]'
+    start = 0 if span[0] - 1 < 0 else span[0] - 1
+    end = len(lines[1]) if span[1] + 1 > len(lines[1]) else span[1] + 1
+    for line in lines:
+        if re.search(symbols_patter, line[start:end]):
+            if lines[1][start:end].startswith("+") or lines[1][start:end].startswith("-"):
+                lines_to_print = [line[start:end] for line in lines]
+                print(lines_to_print)
+                return False
+            return True
+    return False
+
+
+def get_sum_of_all_part_numbers(input_text: str) -> int:
+    result = 0
+    lines = input_text.splitlines()
+    for idx, line in enumerate(lines):
+        lines_to_check = [
+            lines[idx - 1] if idx != 0 else "",
+            line,
+            lines[idx + 1] if idx != len(lines) - 1 else ""
+        ]
+        numbers = re.findall(r'\d+', line)
+        for number in numbers:
+            span = re.search(number, line).span()
+            if is_number_valid(span, lines_to_check):
+                result += int(number)
+    return result
+
+
+with open('input.txt', "r") as file:
+    text = file.read()
+    sum_of_numbers = get_sum_of_all_part_numbers(text)
+    print(sum_of_numbers)
+
+sample = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+"""
+sum_of_numbers = get_sum_of_all_part_numbers(sample)
+print(sum_of_numbers)
+
+
+# 530540 >
+#
+# 528665 <
