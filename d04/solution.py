e924b1ead0262562b245d0e291d053b298c77dbc
Vladyslav Fil <wladyslawfil@gmail.com>
2023-12-04 10:18:19 +0200

    Complete part1 on d04

---
+++ d04/solution.py
@@ -0,0 +1,32 @@
+import functools
+
+
+def get_cards_total_points(input_text: str) -> int:
+    result = 0
+    lines = input_text.splitlines()
+    for line in lines:
+        winning_numbers, numbers = line.split(':')[1].split('|')
+        winning_numbers = set([int(x) for x in winning_numbers.split()])
+        numbers = set([int(x) for x in numbers.split()])
+        intersections = winning_numbers.intersection(numbers)
+        if any(intersections):
+            result += functools.reduce(lambda x, y: x * 2, range(1, len(intersections)), 1)
+    return result
+
+
+with open('input.txt', "r") as file:
+    text = file.read()
+    total_points = get_cards_total_points(text)
+    print(total_points)
+
+
+sample = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
+
+total_points = get_cards_total_points(sample)
+print(total_points)
