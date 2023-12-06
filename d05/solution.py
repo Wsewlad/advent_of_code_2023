341c850e4b7543a57e00179f8d5d0056d0190eef
Vladyslav Fil <wladyslawfil@gmail.com>
2023-12-06 09:56:17 +0200

    Complete part2 of d05 (but not efficient and can't solve the input)

--- d05/solution.py
+++ d05/solution.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 
 def get_mapping(text_input: str) -> tuple:
     name, *rows = text_input.splitlines()
@@ -6,7 +8,7 @@ def get_mapping(text_input: str) -> tuple:
     for row in rows:
         values = [int(n) for n in row.split()]
         ranges.append([values[1], values[0], values[-1]])
-    return name, ranges
+    return name, sorted(ranges, key=lambda x: x[0])
 
 
 def parse_mappings(text_input: str) -> dict:
@@ -17,29 +19,60 @@ def parse_mappings(text_input: str) -> dict:
     return name_to_ranges
 
 
-def map_value(value: int, ranges: dict) -> int:
-    for source, destination, length in ranges:
-        end = source + length
-        if range(source, end).__contains__(value):
-            return range(destination, destination + length)[value - source]
-    return value
+def map_value(value: int, ranges: list) -> int:
+    try:
+        if ranges[0][0] <= value < ranges[-1][0] + ranges[-1][2]:
+            for source, destination, length in ranges:
+                if source <= value < source + length:
+                    return destination + value - source
+        else:
+            return value
+    except:
+        return value
 
 
-def find_the_lowest_location_number(text_input: str) -> int:
-    lowest_location = 0
+def seed_to_location(seed: int, mapping: dict) -> int:
+    soil = map_value(seed, mapping["seed-to-soil"])
+    fertilizer = map_value(soil, mapping["soil-to-fertilizer"])
+    water = map_value(fertilizer, mapping["fertilizer-to-water"])
+    light = map_value(water, mapping["water-to-light"])
+    temperature = map_value(light, mapping["light-to-temperature"])
+    humidity = map_value(temperature, mapping["temperature-to-humidity"])
+    location = map_value(humidity, mapping["humidity-to-location"])
+    return location
+
+
+def part1(text_input: str) -> int:
     seeds = [int(s) for s in text_input.split('seeds:')[1].split('\n')[0].split()]
-    name_to_ranges = parse_mappings(text_input)
+    mapping = parse_mappings(text_input)
+    lowest_location = 0
     for seed in seeds:
-        soil = map_value(seed, name_to_ranges["seed-to-soil"])
-        fertilizer = map_value(soil, name_to_ranges["soil-to-fertilizer"])
-        water = map_value(fertilizer, name_to_ranges["fertilizer-to-water"])
-        light = map_value(water, name_to_ranges["water-to-light"])
-        temperature = map_value(light, name_to_ranges["light-to-temperature"])
-        humidity = map_value(temperature, name_to_ranges["temperature-to-humidity"])
-        location = map_value(humidity, name_to_ranges["humidity-to-location"])
+        location = seed_to_location(seed, mapping)
         if location < lowest_location or lowest_location == 0:
             lowest_location = location
     return lowest_location
+
+
+def part2(text_input: str) -> int:
+    seeds = [int(s) for s in text_input.split('seeds:')[1].split('\n')[0].split()]
+    mapping = parse_mappings(text_input)
+    seed_to_range = {seed: seeds[idx + 1] for idx, seed in enumerate(seeds) if (idx + 1) % 2 != 0}
+    seeds = [
+        range(initial_seed, initial_seed + length) for initial_seed, length in seed_to_range.items()
+    ]
+    # seeds = [
+    #     (initial_seed + idx for idx, x in enumerate(range(length))) for initial_seed, length in seed_to_range.items()
+    # ]
+    seeds = chain.from_iterable(seeds)
+    return min(
+        (seed_to_location(x, mapping) for x in seeds)
+    )
+    # return min(
+    #     (
+    #         min((seed_to_location(x, mapping) for x in s)) for s
+    #         in seeds
+    #     )
+    # )
 
 
 sample = """seeds: 79 14 55 13
@@ -77,10 +110,18 @@ humidity-to-location map:
 56 93 4
 """
 
-result = find_the_lowest_location_number(sample)
+result = part1(sample)
+print(result)
+# #
+# with open('input.txt', "r") as file:
+#     text = file.read()
+#     result = part1(text)
+#     print(result)
+
+result = part2(sample)
 print(result)
 
 with open('input.txt', "r") as file:
     text = file.read()
-    result = find_the_lowest_location_number(text)
+    result = part2(text)
     print(result)
