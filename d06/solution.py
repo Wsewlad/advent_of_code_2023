26c376c98b175678cebe92c850bdb2567f874b02
Vladyslav Fil <wladyslawfil@gmail.com>
2023-12-06 14:02:45 +0200

    Complete part2 of d06

--- d06/solution.py
+++ d06/solution.py
@@ -1,27 +1,37 @@
 import numpy as np
 
 
-def find_number_of_ways_to_beat_the_record(input: str) -> int:
-    lines = [map(lambda x: int(x), line.split(":")[1].split()) for line in input.splitlines()]
-    races = list(zip(*lines))
-    ways_to_beat_the_record = [
-        len(
-            [m for m in range(race[0] - 1) if m * (race[0] - m) > race[1]]
-        )
-        for race in races
-    ]
-    return np.prod(ways_to_beat_the_record)
+def find_number_of_ways_to_beat_the_record(races: list) -> int:
+    return np.prod(
+        [
+            len(
+                [m for m in range(race[0] - 1) if m * (race[0] - m) > race[1]]
+            )
+            for race in races
+        ]
+    )
 
 
 sample = """Time:      7  15   30
 Distance:  9  40  200
 """
 
-print(
-    find_number_of_ways_to_beat_the_record(sample)
-)
 
+def part_1(input_text: str) -> int:
+    lines = [map(lambda x: int(x), line.split(":")[1].split()) for line in input_text.splitlines()]
+    races = list(zip(*lines))
+    return find_number_of_ways_to_beat_the_record(races)
+
+
+def part_2(input_text: str) -> int:
+    race = [int(line.split(":")[1].replace(" ", "")) for line in input_text.splitlines()]
+    return find_number_of_ways_to_beat_the_record([(race[0], race[1])])
+
+
+# print(part_1(sample))
+# with open("input.txt", "r") as f:
+#     print(part_1(f.read()))
+
+print(part_2(sample))
 with open("input.txt", "r") as f:
-    print(
-        find_number_of_ways_to_beat_the_record(f.read())
-    )
+    print(part_2(f.read()))
